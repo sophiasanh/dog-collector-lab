55223acd4307eadb9517e18e1b961e3356f62f13
Sophia Sanh <Sophiasanhbizcoach@gmail.com>
2020-02-10 12:01:18 -0800

    final commit

--- main_app/models.py
+++ main_app/models.py
@@ -28,6 +28,7 @@ class Dog(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
