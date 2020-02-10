18c9e7af929bafc381f5aa3dae8fc419e4c7affa
Sophia Sanh <Sophiasanhbizcoach@gmail.com>
2020-02-09 23:46:19 -0800

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Dog
+
+# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello Dog</h1>')
+def about(request):
+    return render(request, 'about.html')
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog})
+
